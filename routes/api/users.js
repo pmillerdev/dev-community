5ec45e8139beb2bc780491450647951bc184c895
Pete <pmillerdev@gmail.com>
2019-04-19 13:53:27 +0100

    feat: implement login ability

--- routes/api/users.js
+++ routes/api/users.js
@@ -41,4 +41,28 @@ router.post("/register", (req, res) => {
   });
 });
 
+// @route  GET api/users/login
+// @desc   Login User / Return JWT
+// @access Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    // Check for user
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+
+    // Check Password
+    bcrypt.compare(password, user.password).then(isMatched => {
+      if (isMatched) {
+        res.json({ msg: "Login Successful" });
+      } else {
+        return res.status(400).json({ password: "Password incorrect" });
+      }
+    });
+  });
+});
 module.exports = router;

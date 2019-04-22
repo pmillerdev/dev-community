8019e49022611e21941cfe327f1215aec47092cc
Pete <pmillerdev@gmail.com>
2019-04-22 11:11:19 +0100

    feat: route for creating forum posts

--- models/Post.js
+++ models/Post.js
@@ -1,10 +1,10 @@
-const mongoose = requre("mongoose");
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Create Schema
 const PostSchema = new Schema({
   user: {
-    type: Schema.Types.ObjectID,
+    type: Schema.Types.ObjectId,
     ref: "users"
   },
   text: {
@@ -20,7 +20,7 @@ const PostSchema = new Schema({
   likes: [
     {
       user: {
-        type: Schema.Types.ObjectID,
+        type: Schema.Types.ObjectId,
         ref: "users"
       }
     }
@@ -28,7 +28,7 @@ const PostSchema = new Schema({
   comments: [
     {
       user: {
-        type: Schema.Types.ObjectID,
+        type: Schema.Types.ObjectId,
         ref: "users"
       },
       text: {
---
+++ validation/post.js
@@ -0,0 +1,23 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = validatePostInput = data => {
+  let errors = {};
+
+  let { text } = data;
+
+  text = !isEmpty(text) ? text : "";
+
+  if (!Validator.isLength(text, { min: 10, max: 300 })) {
+    errors.text = "Post must be between 10 and 300 characters";
+  }
+
+  if (Validator.isEmpty(text)) {
+    errors.text = "Text field is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};

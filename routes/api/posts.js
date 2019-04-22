25f373d39c4905220149b63023510568dea0e748
Pete <pmillerdev@gmail.com>
2019-04-22 12:00:09 +0100

    feat: allow a user to add and delete comments on a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -42,6 +42,32 @@ router.get("/:id", (req, res) => {
     );
 });
 
+// @route  POST api/posts
+// @desc   Create a post
+// @access Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+      // If any errors send 400 with errors object
+      returnres.status(400).json(errors);
+    }
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
+);
+
 // @route  DELETE api/posts/:id
 // @desc   Delete a single post by id
 // @access Private
@@ -139,11 +165,11 @@ router.post(
   }
 );
 
-// @route  POST api/posts
-// @desc   Create a post
+// @route  POST api/posts/comment/:id
+// @desc   Add a comment to a post
 // @access Private
 router.post(
-  "/",
+  "/comment/:id",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
     const { errors, isValid } = validatePostInput(req.body);
@@ -153,15 +179,54 @@ router.post(
       // If any errors send 400 with errors object
       returnres.status(400).json(errors);
     }
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id
+        };
 
-    const newPost = new Post({
-      text: req.body.text,
-      name: req.body.name,
-      avatar: req.body.avatar,
-      user: req.user.id
-    });
+        // Add to comments array
+        post.comments.unshift(newComment);
 
-    newPost.save().then(post => res.json(post));
+        // Save
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+  }
+);
+
+// @route  DELETE api/posts/comment/:id/:comment_id
+// @desc   Remove a comment from a post
+// @access Private
+router.delete(
+  "/comment/:id/:comment_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        if (
+          post.comments.filter(
+            comment => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          return res
+            .status(404)
+            .json({ commentnotfound: "Comment does not exist " });
+        }
+
+        // Get remove index
+        const removeIndex = post.comments
+          .map(item => item._id.toString())
+          .indexOf(req.params.comment_id);
+
+        post.comments.splice(removeIndex, 1);
+
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: "No post found" }));
   }
 );
 

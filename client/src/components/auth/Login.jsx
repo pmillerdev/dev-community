b6350c7fd5a07d123701e1e60d5a1e5f1ddd113d
Pete <pmillerdev@gmail.com>
2019-04-22 14:16:17 +0100

    refactor: use arrow functions for form events to avoid bind(this)

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -8,18 +8,15 @@ class Login extends Component {
       password: "",
       errors: {}
     };
-
-    this.onChange = this.onChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
   }
 
-  onChange(e) {
+  onChange = e => {
     this.setState({
       [e.target.name]: e.target.value
     });
-  }
+  };
 
-  onSubmit(e) {
+  onSubmit = e => {
     e.preventDefault();
 
     const user = {
@@ -28,7 +25,7 @@ class Login extends Component {
     };
 
     console.log(user);
-  }
+  };
 
   render() {
     return (

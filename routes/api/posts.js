5be8962814426a3b19bc36b417bc9a6eaf2f4a92
Pete <pmillerdev@gmail.com>
2019-04-22 11:17:33 +0100

    feat: get a single post by its id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -23,7 +23,20 @@ router.get("/", (req, res) => {
       date: -1
     })
     .then(posts => res.json(posts))
-    .catch(err => res.status(404));
+    .catch(err =>
+      res.status(404).json({ nopostsfound: "No posts were found" })
+    );
+});
+
+// @route  GET api/posts/:id
+// @desc   Get a single post by id
+// @access Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ nopostfound: "No post found with that id" })
+    );
 });
 
 // @route  POST api/posts

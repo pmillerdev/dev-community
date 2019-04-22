e229a13c6e334785a7958440178c051de888dbf3
Pete <pmillerdev@gmail.com>
2019-04-22 10:52:22 +0100

    feat: create a model/schema for the forum posts

---
+++ models/Post.js
@@ -0,0 +1,56 @@
+const mongoose = requre("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectID,
+    ref: "users"
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectID,
+        ref: "users"
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectID,
+        ref: "users"
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Post = mongoose.model("post", PostSchema);

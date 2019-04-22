7f0d18951f741b583124b9d20f1ec2ae4a22725d
Pete <pmillerdev@gmail.com>
2019-04-22 17:19:44 +0100

    feat: action and reducer to login using jwt token

---
+++ client/src/validation/is-empty.js
@@ -0,0 +1,8 @@
+// Utility function that checks if a field is empty in any way
+const isEmpty = value =>
+  value === undefined ||
+  value === null ||
+  (typeof value === "object" && Object.keys(value).length === 0) ||
+  (typeof value === "string" && value.trim().length === 0);
+
+export default isEmpty;

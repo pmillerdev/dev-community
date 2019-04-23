8e4d33d5fa7f79b37f1dfe422977803d6010929e
Pete <pmillerdev@gmail.com>
2019-04-23 14:03:07 +0100

    feat: create profile actions and reducer

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { clearCurrentProfile } from "./actions/profileActions";
 
 import { Provider } from "react-redux";
 import store from "./store";
@@ -11,6 +12,7 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Dashboard from "./components/dashboard/Dashboard";
 import Footer from "./components/layout/Footer";
 
 import "./App.css";
@@ -30,6 +32,7 @@ if (localStorage.jwtToken) {
     // Logout the user
     store.dispatch(logoutUser());
     // Clear current profile
+    store.dispatch(clearCurrentProfile());
     // Redirect to login page
     window.location.href = "/login";
   }
@@ -46,6 +49,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />
             </div>
             <Footer />
           </div>

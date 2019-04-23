534a512ee7bdc400bc070d9018b194b9f58fac08
Pete <pmillerdev@gmail.com>
2019-04-23 13:25:28 +0100

    refactor: put text inputs in to a component

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -82,7 +82,7 @@ class Navbar extends Component {
 }
 
 Navbar.propTypes = {
-  logoutuser: PropTypes.func.isRequired,
+  logoutUser: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired
 };
 

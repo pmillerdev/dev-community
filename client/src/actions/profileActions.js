8b022ef65cb6588363d530ea42638a551974acaf
Pete <pmillerdev@gmail.com>
2019-04-26 10:13:30 +0100

    feat: create profile actions page and delete profile functionality

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -4,7 +4,8 @@ import {
   GET_PROFILE,
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
-  GET_ERRORS
+  GET_ERRORS,
+  SET_CURRENT_USER
 } from "./types";
 
 // Get current profile
@@ -29,9 +30,29 @@ export const getCurrentProfile = () => dispatch => {
 // Create Profile
 export const createProfile = (profileData, history) => dispatch => {
   axios
-    .post("./api/profile", profileData)
+    .post("/api/profile", profileData)
     .then(res => history.push("/dashboard"))
     .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }));
+};
+
+// Delete account & profile
+export const deleteAccount = () => dispatch => {
+  if (window.confirm("Are you 100% sure? This can NOT be undone")) {
+    axios
+      .delete("/api/profile")
+      .then(res =>
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {}
+        })
+      )
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      );
+  }
 };
 
 // Profile loading

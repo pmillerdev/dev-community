22be58e93a6eb8b847a9f5c87d9cd5bdafccc6ef
Pete <pmillerdev@gmail.com>
2019-04-26 17:16:32 +0100

    feat: github component to display data from github api

--- client/src/components/profile/ProfileGithub.jsx
+++ client/src/components/profile/ProfileGithub.jsx
@@ -1,13 +1,80 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 class ProfileGithub extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cientId: "3c9d632ed84a43aa81fc",
+      clientSecret: "7342423d933bcb8df7c118518de4bc0a819a42ba",
+      count: 5,
+      sort: "created: asc",
+      repos: []
+    };
+  }
+
+  componentDidMount() {
+    const { username } = this.props;
+    const { count, sort, clientId, clientSecret } = this.state;
+
+    fetch(
+      `https:api.github.com/users/${username}/repos?per_page=${count}&sort=${sort}&client_id=${clientId}&client_secret=${clientSecret}`
+    )
+      .then(res => res.json())
+      .then(data => {
+        if (this.refs.gitRef) {
+          this.setState({ repos: data });
+        }
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
+    const { repos } = this.state;
+
+    const repoItems = repos.map(repo => (
+      <div key={repo.id} className="card card-body mb-2">
+        <div className="row">
+          <div className="col-md-6">
+            <h4>
+              <Link
+                to={repo.html_url}
+                className="text-info"
+                target="_blank"
+                rel="noopener noreferer"
+              >
+                {repo.name}
+              </Link>
+            </h4>
+            <p>{repo.description}</p>
+          </div>
+          <div className="col-md-6">
+            <span className="badge badge-info mr-1">
+              Stars: {repo.stargazers_count}
+            </span>
+            <span className="badge badge-secondary mr-1">
+              Watchers: {repo.watchers_count}
+            </span>
+            <span className="badge badge-success mr-1">
+              Forks: {repo.forks_count}
+            </span>
+          </div>
+        </div>
+      </div>
+    ));
     return (
-      <div>
-        <h1>Profile Github Here</h1>
+      <div ref="gitRef">
+        <hr />
+        <h3 className="mb-4">Latest Github Repos</h3>
+        {repoItems}
       </div>
     );
   }
 }
+
+ProfileGithub.propTypes = {
+  username: PropTypes.string.isRequired
+};
 
 export default ProfileGithub;
